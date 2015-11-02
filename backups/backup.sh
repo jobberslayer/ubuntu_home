7149e4f1a84dd42957b3040ce21b838ab2efda4b
Kevin Lester <kevin@Kevins-Mac-mini.local>
2015-11-02 14:43:09 -0500

    getting mailing to work on mac

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,4 +1,9 @@
-cd /home/kevin/backups/e-kevin
-rsync -avz sedawk@e-kevin.com:backups/dbs/ dbs
-rsync -avz sedawk@e-kevin.com:/home/sites/ web
-rsync -avz sedawk@e-kevin.com:/home/rails/ rails
+cd /Users/kevin/backups/e-kevin
+rm /Users/kevin/backups/e-kevin.log
+rm /Users/kevin/backups/e-kevin.err
+echo "LOGS RUN TIME: `date`" >> /Users/kevin/backups/e-kevin.log
+echo "---------------------------------------------" >> /Users/kevin/backups/e-kevin.err
+echo "ERRORS RUN TIME: `date`" >> /Users/kevin/backups/e-kevin.err
+rsync -avz sedawk@e-kevin.com:backups/dbs/ dbs >> /Users/kevin/backups/e-kevin.log 2>> /Users/kevin/backups/e-kevin.err
+rsync -avz sedawk@e-kevin.com:/home/sites/ web >> /Users/kevin/backups/e-kevin.log 2>> /Users/kevin/backups/e-kevin.err
+rsync -avz sedawk@e-kevin.com:/home/rails/ rails >> /Users/kevin/backups/e-kevin.log 2>> /Users/kevin/backups/e-kevin.err
