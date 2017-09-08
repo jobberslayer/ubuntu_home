78eaffa45b44a052372f714fadc64691f559607b
Kevin Lester <kevin@e-kevin.com>
2017-09-08 12:32:56 -0400

    better scratchpad usage

--- .i3/scripts/scratch_pad_windows.sh
+++ .i3/scripts/scratch_pad_windows.sh
@@ -4,4 +4,5 @@ sleep 5
 
 i3-msg exec gnome-calculator
 i3-msg exec /usr/bin/spotify
-i3-msg exec 'terminator -c ranger -x ranger ~'
+i3-msg exec 'terminator -T ranger -x ranger ~'
+i3-msg exec 'gnome-system-monitor'
