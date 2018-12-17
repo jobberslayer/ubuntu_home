1090e8a427be686ed3e0050847baba0ae56fcb9a
Kevin Lester <kevin@e-kevin.com>
2018-12-17 18:50:43 -0500

    install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo Setup vim
+ln -s ~/ubuntu_home/.vimrc ~/.
+ln -s ~/ubuntu_home/.vim ~/.
+echo Setup i3
+ln -s ~/ubuntu_home/.i3 ~/.
+echo Setup Xmodmap
+ln -s ~/ubuntu_home/.Xmodmap ~/.
+echo Setup Xresources
+ln -s ~/ubuntu_home/.Xresources ~/.
+echo Setup terminator
+ln -s ~/ubuntu_home/ubuntu_home/terminator/ ~/.config/.
+
